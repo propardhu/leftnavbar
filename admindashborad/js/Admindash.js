7e4c7804d4388dd12a51acccadb22272dba17175
pardhu <parthasai88@gmail.com>
2020-07-21 11:52:44 +0530

    admin adding

---
+++ admindashborad/js/Admindash.js
@@ -0,0 +1,205 @@
+jQuery(function ($) {
+
+    $(".sidebar-dropdown > a").click(function() {
+  $(".sidebar-submenu").slideUp(200);
+  if (
+    $(this)
+      .parent()
+      .hasClass("active")
+  ) {
+    $(".sidebar-dropdown").removeClass("active");
+    $(this)
+      .parent()
+      .removeClass("active");
+  } else {
+    $(".sidebar-dropdown").removeClass("active");
+    $(this)
+      .next(".sidebar-submenu")
+      .slideDown(200);
+    $(this)
+      .parent()
+      .addClass("active");
+  }
+});
+
+$("#close-sidebar").click(function() {
+  $(".page-wrapper").removeClass("toggled");
+});
+$("#show-sidebar").click(function() {
+  $(".page-wrapper").addClass("toggled");
+});
+});
+new Chart(document.getElementById("line-chart"), {
+  type: 'line',
+  data: {
+    labels: ["Monday","Thuesday","Wednesday","Thursday","Friday","Saturday","Sunday"],
+    datasets: [{
+        data: [86,114,106,106,107,111,133],
+        label: "Total collection",
+        borderColor: "#3e95cd",
+        fill: true
+      },{
+        data: [168,170,178,190,203,276,408],
+        label: "fees collection",
+        borderColor: "#3cba9f",
+        fill: true
+      }
+    ]
+  }
+
+});
+
+// Bar chart
+new Chart(document.getElementById("bar-chart"), {
+    type: 'bar',
+    data: {
+      labels: ["jan", "feb", "mar"],
+      datasets: [
+        {
+          label: "Expenses",
+          backgroundColor: ["#3cba9f","#e8c3b9","#c45850"],
+          data: [2478,5267,734]
+        }
+      ]
+    },
+    options: {
+      legend: { display: false }
+
+    }});
+
+    new Chart(document.getElementById("doughnut-chart"), {
+        type: 'doughnut',
+        data: {
+          labels: ["Present","Absent"],
+          datasets: [
+            {
+              label: "Population (millions)",
+              backgroundColor: [ "#8e5ea2","#3cba9f"],
+              data: [14,86]
+            }
+          ]
+        }
+
+    });
+
+    new Chart(document.getElementById("mixed-chart"), {
+        type: 'bar',
+        data: {
+          labels: ["1900", "1950", "1999", "2050"],
+          datasets: [{
+              label: "Europe",
+              type: "line",
+              borderColor: "#8e5ea2",
+              data: [408,547,675,734],
+              fill: false
+            }, {
+              label: "Africa",
+              type: "line",
+              borderColor: "#3e95cd",
+              data: [133,221,783,2478],
+              fill: false
+            }, {
+              label: "Europe",
+              type: "bar",
+              backgroundColor: "rgba(0,0,0,0.2)",
+              data: [408,547,675,734],
+            }, {
+              label: "Africa",
+              type: "bar",
+              backgroundColor: "rgba(0,0,0,0.2)",
+              backgroundColorHover: "#3e95cd",
+              data: [133,221,783,2478]
+            }
+          ]
+        },
+        options: {
+          title: {
+            display: true,
+            text: 'Population growth (millions): Europe & Africa'
+          },
+          legend: { display: false }
+        }
+    });
+
+
+let today = new Date();
+let currentMonth = today.getMonth();
+let currentYear = today.getFullYear();
+let selectYear = document.getElementById("year");
+let selectMonth = document.getElementById("month");
+
+let months = ["Jan", "Feb", "Mar", "Apr", "May", "Jun", "Jul", "Aug", "Sep", "Oct", "Nov", "Dec"];
+
+let monthAndYear = document.getElementById("monthAndYear");
+showCalendar(currentMonth, currentYear);
+
+
+function next() {
+    currentYear = (currentMonth === 11) ? currentYear + 1 : currentYear;
+    currentMonth = (currentMonth + 1) % 12;
+    showCalendar(currentMonth, currentYear);
+}
+
+function previous() {
+    currentYear = (currentMonth === 0) ? currentYear - 1 : currentYear;
+    currentMonth = (currentMonth === 0) ? 11 : currentMonth - 1;
+    showCalendar(currentMonth, currentYear);
+}
+
+function jump() {
+    currentYear = parseInt(selectYear.value);
+    currentMonth = parseInt(selectMonth.value);
+    showCalendar(currentMonth, currentYear);
+}
+
+function showCalendar(month, year) {
+
+    let firstDay = (new Date(year, month)).getDay();
+    let daysInMonth = 32 - new Date(year, month, 32).getDate();
+
+    let tbl = document.getElementById("calendar-body"); // body of the calendar
+
+    // clearing all previous cells
+    tbl.innerHTML = "";
+
+    // filing data about month and in the page via DOM.
+    monthAndYear.innerHTML = months[month] + " " + year;
+    selectYear.value = year;
+    selectMonth.value = month;
+
+    // creating all cells
+    let date = 1;
+    for (let i = 0; i < 6; i++) {
+        // creates a table row
+        let row = document.createElement("tr");
+
+        //creating individual cells, filing them up with data.
+        for (let j = 0; j < 7; j++) {
+            if (i === 0 && j < firstDay) {
+                let cell = document.createElement("td");
+                let cellText = document.createTextNode("");
+                cell.appendChild(cellText);
+                row.appendChild(cell);
+            }
+            else if (date > daysInMonth) {
+                break;
+            }
+
+            else {
+                let cell = document.createElement("td");
+                let cellText = document.createTextNode(date);
+                if (date === today.getDate() && year === today.getFullYear() && month === today.getMonth()) {
+                    cell.classList.add("bg-info");
+                } // color today's date
+                cell.appendChild(cellText);
+                row.appendChild(cell);
+                date++;
+            }
+
+
+        }
+
+        tbl.appendChild(row); // appending each row into calendar body.
+    }
+
+}
